52d03b33b0da19e0a60165723675946abb537621
georg <georgy.gibizov@gmail.com>
2025-07-16 17:27:37 +0300

    tested pull + push

--- app/src/main/java/com/example/lifeinpoints/MainActivity.kt
+++ app/src/main/java/com/example/lifeinpoints/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LifeInPointsTheme {
-        Greeting("Jojo")
+        Greeting("Hello world!")
     }
 }
