870fd72772883bedf0506f5f14922e1d48260314
Koliasik <miroshnikov.nikolai.a@gmail.com>
2025-07-16 17:22:07 +0300

    change title

--- app/src/main/java/com/example/lifeinpoints/MainActivity.kt
+++ app/src/main/java/com/example/lifeinpoints/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             LifeInPointsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "JOJO",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LifeInPointsTheme {
-        Greeting("Android")
+        Greeting("Jojo")
     }
 }
